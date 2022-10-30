afaff0ae645a86eb431d9ca3c5c4c1ecd4e5326d
Sam Watson <multisniperism@gmail.com>
2022-10-30 16:45:29 +1300

    remove debug from release profile

--- examples/loop.asm
+++ examples/loop.asm
@@ -1,4 +1,5 @@
 sym i 0 ; define symbol "i" with value 0
+sym epoch getepoch
 
 label main
     print getsym i ; print value of symbol "i"
@@ -12,5 +13,13 @@ label main
     sym i stacktop ; store value on stack into symbol "i"
 
     jlt main ; go back to the start if i is less than limit
+
+    push getsym epoch
+    push getepoch
+    sub
+
+    print "\n100,000 itertions took "
+    print stacktop
+    print "ms\n"
 
     exit 0
